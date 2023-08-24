9d553ba29659ba41722a00a868e53109d51a1f86
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-08-24 18:12:27 -0400

    Finished listing question score if statements

--- node.js
+++ node.js
@@ -49,9 +49,32 @@ app.get("/", function(req,res) {
             if (result.question15 === true) score +=1
             if (result.question16 === true) score +=1
             if (result.question17 === true) score +=1
-            if (result.question18a === "Several days") score +=1 // Probably need to create a separate score variable to add 18a and 18b together to assign if its high risk or not
-            if (result.question18b === "More than half the days") score +=2
-            if (result.question18c === "Nearly every day") score +=3
+            if (result.question18aa === "Several days") score +=1 // Change equivelancy check based on actual data
+            if (result.question18ab === "More than half the days") score +=2
+            if (result.question18ac === "Nearly every day") score +=3
+            if (result.question18ba === "Several days") score +=1 
+            if (result.question18bb === "More than half the days") score +=2
+            if (result.question18bc === "Nearly every day") score +=3
+            if (result.question19a === "A little bit") score+=1
+            if (result.question19b === "Somewhat") score+=2
+            if (result.question19c === "Quite a bit") score+=3
+            if (result.question19d === "Very much") score+=4
+            if (result.question20aa === "Once or Twice") score +=1  // For naming convention, might be better to count the questions w/ same number as another one (18b -> 19, 20b -> 21))
+            if (result.question20ab === "Monthly") score +=2
+            if (result.question20ac === "Weekly") score +=3
+            if (result.question20ad === "Daily or Almost Daily") score +=4
+            if (result.question20ba === "Once or Twice") score +=1
+            if (result.question20bb === "Monthly") score +=2
+            if (result.question20bc === "Weekly") score +=3
+            if (result.question20bd === "Daily or Almost Daily") score +=4
+            if (result.question20ca === "Once or Twice") score +=1
+            if (result.question20cb === "Monthly") score +=2
+            if (result.question20cc === "Weekly") score +=3
+            if (result.question20cd === "Daily or Almost Daily") score +=4
+            if (result.question20da === "Once or Twice") score +=1
+            if (result.question20db === "Monthly") score +=2
+            if (result.question20dc === "Weekly") score +=3
+            if (result.question20dd === "Daily or Almost Daily") score +=3
         scores.push(score)
         }
     console.log(scores)
@@ -61,3 +84,9 @@ app.post("/", (req)=> {
     req.body.command()
     }
 );
+
+
+// If we need to identify medium/high/imminent risk for each category
+// Create variable for each category, assigned 0 by default
+// Add to that value whenever the results are checked
+// Same idea for the grouped categories like 18 and 20
